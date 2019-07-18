ea21794cd8cb45432ceaf181216bd677313f18a6
itsdevkr <38876728+itsdevkr@users.noreply.github.com>
2019-07-18 06:55:43 +0530

    Update AllPermutations.py

--- AllPermutations.py
+++ AllPermutations.py
@@ -2,5 +2,13 @@ from itertools import permutations
 s=input()
 s=list(s)
 perm=permutations(s)
+a=[]
 for j in list(perm):
-	print(''.join(list(j)))
+	a.append(''.join(list(j)))
+b=list(set(a))
+c=[]
+for i in range(len(b)):
+	c.append(b[i])
+c.sort()
+for i in range(len(c)):
+	print(c[i])
