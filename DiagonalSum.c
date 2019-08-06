5b4665ef83e6ceeb379953783d6c64e534359062
itsdevkr <38876728+itsdevkr@users.noreply.github.com>
2019-08-07 00:24:20 +0530

    Create DiagonalSum.c

---
+++ DiagonalSum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,j,s=0;
+	scanf("%d",&n);
+	int A[n][n];
+	for(i=0;i<n;i++)
+	for(j=0;j<n;j++)
+	scanf("%d",&A[i][j]);
+	for(i=0;i<n;i++)
+	for(j=0;j<n;j++)
+	if(i==j || i+j==n-1)
+	{
+		s=s+A[i][j];
+	}
+	printf("%d",s);
+	return 0;
+}
