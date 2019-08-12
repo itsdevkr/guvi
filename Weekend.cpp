84e13d63302a3103bc0b6943c4711912b0747e8c
itsdevkr <38876728+itsdevkr@users.noreply.github.com>
2019-08-12 12:46:27 +0530

    Create Weekend.cpp

---
+++ Weekend.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string s;
+	cin>>s;
+	if(s=="Saturday" || s=="Sunday")
+	cout<<"yes";
+	else
+	cout<<"no";
+	return 0;
+}
